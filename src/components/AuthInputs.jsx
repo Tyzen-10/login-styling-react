29edc0d0b84c6ec8217f0cb5b2b75e6875c86f31
Venkatesan R S <wenkatesan@gmail.com>
2023-11-08 01:18:30 +0530

    tailwind styled code commit

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { styled } from "styled-components";
 const TestDiv = styled.div`
-  background-color: #3c3a3a;
   border-radius: 0.25rem;
 `;
 const ControlsDiv = styled.div`
@@ -11,6 +10,7 @@ const ControlsDiv = styled.div`
   margin-bottom: 1.5rem;
 `;
 import CustomInput from "./CustomInput";
+import Button from "./Button";
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -32,34 +32,30 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <TestDiv>
-        <ControlsDiv>
-          <CustomInput
-            label="Email"
-            $invalid={emailNotValid}
-            type="email"
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6 "> 
+        <CustomInput
+          label="Email"
+          $invalid={emailNotValid}
+          type="email"
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
 
-          <CustomInput
-            label="Password"
-            $invalid={passwordNotValid}
-            type="password"
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </ControlsDiv>
-        <div className="actions">
-          <button type="button" className="text-button">
-            Create a new account
-          </button>
-          <button className="button" onClick={handleLogin}>
-            Sign In
-          </button>
-        </div>
-      </TestDiv>
+        <CustomInput
+          label="Password"
+          $invalid={passwordNotValid}
+          type="password"
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+      </div>
+      <div className="flex justify-end gap-4 ">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
+          Create a new account
+        </button>
+        <Button onClick={handleLogin}>Sign in</Button>
+      </div>
     </div>
   );
 }
